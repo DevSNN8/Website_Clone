19cbb51a987bf2946222b78f7c07af7d6c8e8377
DevSNN8 <85948511+DevSNN8@users.noreply.github.com>
2024-07-08 09:19:21 +0700

    l

--- script.js
+++ script.js
@@ -1,23 +1,6 @@
-const redBtn = document.getElementById("redBtn");
-const greeBtn = document.getElementById("greeBtn");
-const yelBtn = document.getElementById("yelBtn");
-const defBtn = document.getElementById("defBtn");
-const mbox = document.getElementById("mbox");
+console.log("hello shit world");
 
+document.getElementById("text").innerHTML = "mark";
 
-redBtn.addEventListener("click", () => {
-    mbox.style.backgroundColor ="red";
-})
-
-
-greeBtn.addEventListener("click", () => {
-    mbox.style.backgroundColor ="green";
-})
-
-yelBtn.addEventListener("click", () => {
-    mbox.style.backgroundColor ="yellow";
-})
-
-defBtn.addEventListener("click", () => {
-    mbox.style.backgroundColor ="black";
-})
+document.write("Hello fox");
+alert("Banana");
