f395469a4342aa5518fead378da53571c55462bc
DevSNN8 <85948511+DevSNN8@users.noreply.github.com>
2024-12-30 09:09:08 +0700

    python

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-score = 22
+import tkinter as tk
 
-if score >= 99:
-    print('Win')
-elif score >= 55:
-    print('I thai')
-else: 
-    print('I foot go to hell')
+def set_messsge():
+    text = text_input.get()
+    title_label.configure(text=text)
+
+window = tk.Tk()
+window.title('Jugle Python')
+window.minsize(width= 400, height= 400)
+
+title_label = tk.Label(master=window, text='Hunter Milf')
+title_label.pack()
+
+text_input = tk.Entry(master=window)
+text_input.pack()
+
+btn = tk.Button(master=window, text='OL', command= set_messsge)
+btn.pack()
+
+window.mainloop()
+
