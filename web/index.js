65f1ad844e37c5f2e3c3f9c7399c0bab9ae59380
DevSNN8 <85948511+DevSNN8@users.noreply.github.com>
2025-01-06 09:17:43 +0700

    ll

--- web/index.js
+++ web/index.js
@@ -1,2 +1,7 @@
-let mario = 'Milf'
-console.log(mario)
+function getPyramid (length,width,height) {
+    let base = length * width;
+    let Pyramidv = 1/3 * base * height;
+    console.log(Pyramidv);
+}
+
+getPyramid(8,3,7);
