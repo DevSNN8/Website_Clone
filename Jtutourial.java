4f9bd68044868d0e3146c7fc272719d3c8129170
DevSNN8 <85948511+DevSNN8@users.noreply.github.com>
2025-01-14 07:57:43 +0700

    llll

---
+++ Jtutourial.java
@@ -0,0 +1,5 @@
+class Milf {
+    public static void main(String[] args) {
+        System.out.println("Pussy");
+    }
+}
--- web/index.js
+++
@@ -1,7 +0,0 @@
-function getPyramid (length,width,height) {
-    let base = length * width;
-    let Pyramidv = 1/3 * base * height;
-    console.log(Pyramidv);
-}
-
-getPyramid(8,3,7);
