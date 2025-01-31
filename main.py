ccb1eb11ed258820d7b78b0bef928c9cd79f2a8c
DevSNN8 <85948511+DevSNN8@users.noreply.github.com>
2025-01-31 08:12:50 +0700

    g

--- main.py
+++ main.py
@@ -1,35 +1,7 @@
-import tkinter as tk 
-def show_output():
-    number = int(num_input.get())
-
-    if number == 0:
-        output_r.configure(text='Banana')
-        return
-
-    output = ''
-    for i in range (1,15):
-        output += str(number) + 'X' + str(i)
-        output += '=' + str(number * i) + '\n'
-
-    output_r.configure(text=output)
-
-window = tk.Tk()
-window.title('Content Python')
-window.minsize(width=400, height = 400)
-
-title_label = tk.Label(master=window, text='Brandi Love')
-title_label.pack()
-
-num_input = tk.Entry(master=window)
-num_input.pack()
-
-
-btn = tk.Button(
-    master=window, text='Result',command=show_output
-)
-btn.pack()
-
-output_r = tk.Label(master=window)
-output_r.pack()
-
-window.mainloop()
+book = {
+    'name': 'Python',
+    'price': 40, 
+    'page': 500
+}
+book.pop('page')
+print(book)
